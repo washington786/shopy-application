3321eba152d90babffdbf9467c05092763a3c65d
washington786 <dkmawasha@gmail.com>
2025-09-20 15:13:26 +0200

    feat(backend)-payment: linked payment handling to the controller payment.

--- Backend/Controllers/PaymentController.cs
+++ Backend/Controllers/PaymentController.cs
@@ -1,11 +1,28 @@
+using Backend.Models;
+using Backend.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PaymentController : ControllerBase
+    public class PaymentController(IPaymentService paymentService, UserManager<AppUser> userManager) : ControllerBase
     {
+        private readonly IPaymentService service = paymentService;
+        private readonly UserManager<AppUser> _usermanager = userManager;
+
+        [HttpPost("order-payment")]
+        public async Task<ActionResult> MakeOrderPaymentAsync()
+        {
+            var user = await _usermanager.GetUserAsync(User);
+
+            if (user is null) return Unauthorized();
+
+            var res = await service.CreateCheckOut(user.Id);
+
+            return Ok(res);
+        }
     }
 }
