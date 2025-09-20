b2bb2c7da38a201b1386326da53f661bf7353481
washington786 <dkmawasha@gmail.com>
2025-09-20 03:00:25 +0200

    feat(backend)products: implement full crud operation

--- Backend/Controllers/ProductsController.cs
+++ Backend/Controllers/ProductsController.cs
@@ -1,3 +1,6 @@
+using Backend.DTOs.Requests.Products;
+using Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,7 +8,48 @@ namespace Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController(IProductService productService) : ControllerBase
     {
+        private readonly IProductService service = productService;
+
+        [HttpGet("all-products")]
+        public async Task<ActionResult<string>> GetAllProductsAsync()
+        {
+            var res = await service.GetAllAsync();
+            return Ok(res);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult> GetProductByIdAsync(int productId)
+        {
+            var res = await service.GetByIdAsync(productId);
+            return Ok(res);
+        }
+
+        [Authorize(Roles = "Admin,StoreManager")]
+        [HttpPost("create-product")]
+        public async Task<ActionResult> CreateProductAsync([FromBody] CreateProductRequest createProductRequest)
+        {
+            if (!ModelState.IsValid) return BadRequest(new { message = "invalid input fields" });
+
+            var res = await service.CreateAsync(createProductRequest);
+            return CreatedAtAction(nameof(GetProductByIdAsync), new { id = res.Id }, res);
+        }
+        [Authorize(Roles = "Admin,StoreManager")]
+        [HttpPut("{productId}")]
+        public async Task<ActionResult> UpdateProductAsync(int productId, [FromBody] UpdateProductRequest updateProduct)
+        {
+            if (!ModelState.IsValid) return BadRequest(new { message = "invalid input fields" });
+            var res = await service.UpdateAsync(productId, updateProduct);
+            return Ok(res);
+        }
+        [Authorize(Roles = "Admin,StoreManager")]
+        [HttpDelete("{productId}")]
+        public async Task<ActionResult> DeleteProductAsync(int productId)
+        {
+            await service.RemoveAsync(productId);
+
+            return NoContent();
+        }
     }
 }
