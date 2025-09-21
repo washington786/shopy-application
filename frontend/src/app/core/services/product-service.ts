8c0d8d6548550f976e8baa85e7afe29a2009fc7f
washington786 <dkmawasha@gmail.com>
2025-09-22 01:15:57 +0200

    feat(services): implemented product service functions.

--- frontend/src/app/core/services/product-service.ts
+++ frontend/src/app/core/services/product-service.ts
@@ -1,20 +1,31 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiService } from './api-service';
+import { CreateProductRequest, ProductDto, UpdateProductRequest } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private apiService = inject(ApiService);
-  private productsUrl = `/products/`;
+  private productsUrl = `/products`;
 
-  getAllProducts() { }
+  getAllProducts() {
+    this.apiService.get<ProductDto[]>(`${this.productsUrl}/all-products`);
+  }
 
-  getProduct() { }
+  getProduct(id: number) {
+    return this.apiService.get<ProductDto>(`${this.productsUrl}/${id}`);
+  }
 
-  updateProductDetails() { }
+  updateProductDetails(id: number, request: UpdateProductRequest) {
+    return this.apiService.put<ProductDto>(`${this.productsUrl}/${id}`, request);
+  }
 
-  removeProduct() { }
+  removeProduct(id: number) {
+    return this.apiService.delete<void>(`${this.productsUrl}/${id}`)
+  }
 
-  createProduct() { }
+  createProduct(request: CreateProductRequest) {
+    return this.apiService.post<ProductDto>(`${this.productsUrl}`, request);
+  }
 }
