a661316cedadbc496f66b68c2768717a87c79f8a
washington786 <dkmawasha@gmail.com>
2025-09-22 01:47:24 +0200

    feat(services): implemented admin service functions.

--- frontend/src/app/core/services/admin-service.ts
+++ frontend/src/app/core/services/admin-service.ts
@@ -1,14 +1,20 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiService } from './api-service';
+import { UserDto } from '../models/auth.model';
+import { updateRoleRequest } from '../models/admin.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
   private apiService = inject(ApiService);
-  private authUrl = `/admin/`;
+  private url = `/admin`;
 
-  loadAllUsers() { }
+  loadAllUsers() {
+    return this.apiService.get<UserDto[]>(`/all-users`);
+  }
 
-  updateUserRole() { }
+  updateUserRole(request: updateRoleRequest, id: string) {
+    return this.apiService.put(`${this.url}/user/${id}`, request);
+  }
 }
