b5ac4f7a6d6bd15898f5b16cc4d4f3b3a345aacc
washington786 <dkmawasha@gmail.com>
2025-09-22 22:54:21 +0200

    feat(state): linking stores and effects in config file.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -8,14 +8,22 @@ import { provideRouterStore } from '@ngrx/router-store';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtAuthInterceptor } from './core/interceptor/jwt-auth-interceptor';
 import { errorHandlerInterceptor } from './core/interceptor/error-handler-interceptor';
+import { AuthEffects } from './store/effects/auth-effects';
+import { CartEffects } from './store/effects/cart-effects';
+import { OrderEffects } from './store/effects/order-effects';
+import { ProductEffects } from './store/effects/product-effects';
+import { authReducer } from './store/reducers/auth-reducer';
+import { cartReducer } from './store/reducers/cart-reducer';
+import { orderReducer } from './store/reducers/order-reducer';
+import { productsReducer } from './store/reducers/product-reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideStore(),
-    provideEffects(),
+    provideStore({ auth: authReducer, cart: cartReducer, order: orderReducer, product: productsReducer }),
+    provideEffects([AuthEffects, CartEffects, OrderEffects, ProductEffects]),
     provideRouterStore(),
     provideHttpClient(withInterceptors([jwtAuthInterceptor, errorHandlerInterceptor]))
   ]
