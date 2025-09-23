df0e9f6995b132d3811c47409e782bae4b0320cb
washington786 <dkmawasha@gmail.com>
2025-09-23 16:46:21 +0200

    feat(auth):profile ui integration with service.

--- frontend/src/app/features/auth/profile/profile.ts
+++ frontend/src/app/features/auth/profile/profile.ts
@@ -1,11 +1,17 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingSpinner } from "../../../shared/loading-spinner/loading-spinner";
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { UserDto } from '../../../core/models/auth.model';
+import { selectAuthError, selectAuthLoading, selectAuthUser } from '../../../store/selectors/auth-selector';
+import { logoutAction } from '../../../store/actions/auth-actions';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [ReactiveFormsModule, LoadingSpinner],
+  imports: [ReactiveFormsModule, LoadingSpinner, AsyncPipe],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
@@ -15,17 +21,19 @@ export class Profile {
 
   router = inject(Router);
 
-  isLoading: boolean = false;
-  errorMessage: string | null = null;
+  store = inject(Store);
+
+  destroy = inject(DestroyRef);
+
+  user$!: Observable<UserDto | null>;
+
+  isLoading$!: Observable<boolean>;
+  errorMessage$!: Observable<string | null>;
+
   isEditing: boolean = false;
   successMessage: string | null = null;
 
-  mockUser = {
-    id: '1',
-    email: 'user@example.com',
-    fullName: 'John Doe',
-    roles: ['User']
-  };
+  loadedUser: UserDto | null = null;
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -33,10 +41,20 @@ export class Profile {
       fullName: ['', [Validators.required]]
     });
 
-    this.form.patchValue({
-      email: this.mockUser.email,
-      fullName: this.mockUser.fullName
-    })
+    this.user$ = this.store.select(selectAuthUser);
+    this.isLoading$ = this.store.select(selectAuthLoading);
+    this.errorMessage$ = this.store.select(selectAuthError);
+
+    let sub = this.user$.subscribe({
+      next: (res) => {
+        this.form.patchValue({
+          email: res?.email,
+          fullName: res?.fullName
+        });
+        this.loadedUser = res;
+      }
+    });
+    this.destroy.onDestroy(() => sub.unsubscribe());
   }
 
   toggleEdit() {
@@ -54,6 +72,6 @@ export class Profile {
   }
 
   logout() {
-    this.router.navigate(["/auth/login"]);
+    this.store.dispatch(logoutAction());
   }
 }
